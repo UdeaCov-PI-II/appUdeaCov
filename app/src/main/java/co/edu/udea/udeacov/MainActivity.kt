7d9980ce09baeeefac689f21929a26acbc6b0c19
paula970 <andrea.maldonado@udea.edu.co>
2020-11-03 00:50:06 -0500

    version 1

--- app/src/main/java/co/edu/udea/udeacov/MainActivity.kt
+++ app/src/main/java/co/edu/udea/udeacov/MainActivity.kt
@@ -1,11 +1,22 @@
 package co.edu.udea.udeacov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_registrarse.setOnClickListener {
+            val intent: Intent = Intent(this, preingreso::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
---
+++ app/src/main/java/co/edu/udea/udeacov/preingreso.kt
@@ -0,0 +1,12 @@
+package co.edu.udea.udeacov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class preingreso : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preingreso)
+    }
+    
+}
