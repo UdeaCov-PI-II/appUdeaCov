2c074fd69fe7cc0d21ca84442c4039a3a311e850
paula970 <andrea.maldonado@udea.edu.co>
2020-11-06 00:33:57 -0500

    formulario de preingreso

--- app/src/main/java/co/edu/udea/udeacov/preingreso1.kt
+++ app/src/main/java/co/edu/udea/udeacov/preingreso1.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_preingreso1.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +38,17 @@ class preingreso1 : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_preingreso1, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        preingresobtn_siguiente1.setOnClickListener{
+            it.findNavController().navigate(R.id.action_preingreso1_to_preingreso2)
+        }
+
+    }
+
+
 
     companion object {
         /**
