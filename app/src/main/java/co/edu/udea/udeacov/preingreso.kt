08d6212eced5c6b1ba9403e762922c34c58b9a2e
paula970 <andrea.maldonado@udea.edu.co>
2020-11-23 03:25:16 -0500

    avances1 formulario

--- app/src/main/java/co/edu/udea/udeacov/preingreso.kt
+++ app/src/main/java/co/edu/udea/udeacov/preingreso.kt
@@ -2,12 +2,18 @@ package co.edu.udea.udeacov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_preingreso1.*
 
 class preingreso : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preingreso)
 
+
     }
+
+
     
 }
--- app/src/main/java/co/edu/udea/udeacov/preingreso1.kt
+++ app/src/main/java/co/edu/udea/udeacov/preingreso1.kt
@@ -1,16 +1,19 @@
 package co.edu.udea.udeacov
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.wajahatkarim3.easyvalidation.core.view_ktx.nonEmpty
 import com.wajahatkarim3.easyvalidation.core.view_ktx.onlyNumbers
 import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
 import kotlinx.android.synthetic.main.fragment_preingreso1.*
+import kotlinx.android.synthetic.main.fragment_preingreso1.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +26,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class preingreso1 : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -30,11 +34,28 @@ class preingreso1 : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-
         }
+    }
+
+    private fun validate() :Boolean{
+        if(preingreso1_correo.text.toString().isEmpty()){
+            preingreso1_correo.error = "campo vacío"
+            return false
+        }else if(preingreso1_doc.text.toString().isEmpty()){
+            preingreso1_doc.error = "campo vacío"
+            return false
+        }else if(preingreso1_nombre.text.toString().isEmpty()){
+            preingreso1_nombre.error = "campo vacío"
+            return false
+        }else if(preingreso1_autorizo.isChecked == false){
+            preingreso1_autorizo.error = "Es importante tu información para que la Institución pueda dar cumplimiento a la Resolución 0666 del 24 de abril de 2010 expedida por el Ministerio de Salud y poder orientar acciones preventivas frente al COVID-19."
+            return false
+        }
+        return true
     }
 
     override fun onCreateView(
@@ -45,31 +66,17 @@ class preingreso1 : Fragment() {
         return inflater.inflate(R.layout.fragment_preingreso1, container, false)
     }
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var myEditText = view.findViewById<EditText>(R.id.preingreso1_nombre)
-        var myDoc = view.findViewById<EditText>(R.id.preingreso1_doc)
-        myDoc.onlyNumbers(){
-            myDoc.error = it
-        }
-        myEditText.validator()
-            .nonEmpty()
-            .onlyNumbers()
-            .addErrorCallback { myEditText.error=it }
-            .check()
-
-
-
-
         preingresobtn_siguiente1.setOnClickListener{
-            it.findNavController().navigate(R.id.action_preingreso1_to_preingreso2)
+            if(validate()){
+                Toast.makeText(activity, "Campos diligenciados", Toast.LENGTH_SHORT).show();
+                it.findNavController().navigate(R.id.action_preingreso1_to_preingreso2)
+            }
+
         }
 
     }
-
-
 
     companion object {
         /**
