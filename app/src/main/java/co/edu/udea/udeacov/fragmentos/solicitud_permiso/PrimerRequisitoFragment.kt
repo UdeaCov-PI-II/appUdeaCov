9ef2aaf32f5f9864ef3ca8869cca659944901d2f
paula970 <andrea.maldonado@udea.edu.co>
2021-01-18 12:41:29 -0500

    datos quemados funcionan después de seleccionar el spinner

--- app/src/main/java/co/edu/udea/udeacov/fragmentos/solicitud_permiso/PrimerRequisitoFragment.kt
+++ app/src/main/java/co/edu/udea/udeacov/fragmentos/solicitud_permiso/PrimerRequisitoFragment.kt
@@ -5,9 +5,12 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
--- app/src/main/java/co/edu/udea/udeacov/fragmentos/solicitud_permiso/SolicitudIngreso1.kt
+++ app/src/main/java/co/edu/udea/udeacov/fragmentos/solicitud_permiso/SolicitudIngreso1.kt
@@ -1,10 +1,15 @@
 package co.edu.udea.udeacov.fragmentos.solicitud_permiso
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import co.edu.udea.udeacov.R
 import com.kofigyan.stateprogressbar.StateProgressBar
@@ -28,11 +33,42 @@ class SolicitudIngreso1 : Fragment() {
         return inflater.inflate(R.layout.fragment_solicitud_ingreso1, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         SolicitudIngreso1btn_siguiente1.setOnClickListener {
             it.findNavController().navigate(R.id.action_solicitudIngreso1_to_solicitudIngreso2)
+        }
+
+        var mySpinner = view.findViewById<Spinner>(R.id.SolicitudIngreso1_spinner)
+        var text2 = view.findViewById<TextView>(R.id.decano)
+        var text3 = view.findViewById<TextView>(R.id.correoDecano)
+        mySpinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val text = mySpinner.selectedItem.toString()
+
+                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show();
+                if(text == "Facultad de Artes"){
+                    text2.setText("Gabriel Velez");
+                    text3.setText("gabriel.velez@udea.edu.co");
+
+                }else if(text == "Facultad de Ciencias Agrarias"){
+                    text2.setText("Liliana Mahecha Ledesma");
+                    text3.setText("decaagrarias@udea.edu.co");
+
+                }else if(text == "Facultad de Ciencias Económicas"){
+                    text2.setText("Sergio Iván Restrepo Ochoa");
+                    text3.setText("decaeconomicas@udea.edu.co");
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
     }
 
